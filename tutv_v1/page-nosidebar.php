a5870808155efd0849ad9918019ca4c2c81a3aec
tutv_58 <tutv95@gmail.com>
2014-10-28 10:19:40 +0700

    change page about

--- tutv_v1/page-nosidebar.php
+++ tutv_v1/page-nosidebar.php
@@ -16,6 +16,31 @@
 							</header>
 							<div class="post-content box mark-links">
 								<?php the_content(); ?>
+								<div class="team-bk">
+									<div class="postauthor">
+										<h4><a href="http://tutran.net/author/tutv95" title="Tú Trần" target="_blank">Trần Văn Tú</a></h4>
+										<?php if(function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email', 1), '100' );  } ?>
+										<p><?php the_author_meta('description', 1) ?></p>
+									</div>
+
+									<div class="postauthor">
+										<h4><a href="http://tutran.net/author/quytm_58" title="Trần Minh Quý" target="_blank">Trần Minh Quý</a></h4>
+										<?php if(function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email', 3), '100' );  } ?>
+										<p><?php the_author_meta('description', 3) ?></p>
+									</div>
+
+									<div class="postauthor">
+										<h4><a href="http://tutran.net/author/toonies1810" title="Nguyễn TIến Minh" target="_blank">Nguyễn Tiến Minh</a></h4>
+										<?php if(function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email', 4), '100' );  } ?>
+										<p><?php the_author_meta('description', 4) ?></p>
+									</div>
+
+									<div class="postauthor">
+										<h4><a href="http://tutran.net/author/bmt-uet" title="Bùi Minh Thái" target="_blank">Bùi Minh Thái</a></h4>
+										<?php if(function_exists('get_avatar')) { echo get_avatar( get_the_author_meta('email', 2), '100' );  } ?>
+										<p><?php the_author_meta('description', 2) ?></p>
+									</div>
+								</div>
 								<?php wp_link_pages('before=<div class="pagination">&after=</div>'); ?>
 							</div><!--.post-content box mark-links -->
 						</div><!--.g post-->
